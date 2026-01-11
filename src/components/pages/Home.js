f6fb70ca83fa6fb02adadff1285e65dfbdb0ddea
shine chikwapulo <chikwapuloshine@gmail.com>
2026-01-11 22:13:22 +0200

    Add SEO metadata, sitemap, and robots.txt

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -16,12 +16,13 @@ function Home () {
 
             <Helmet>
             <title>Shine Chikwapulo | Software Engineer</title>
+            <meta name="google-site-verification" content="q2HC-O3rlZcv1OptYuyn_0wLtIm2_vONOTi2voR0mek" />
             <meta
                 name="description"
                 content="My name is Shine Chikwapulo, and I’m a software engineer based in Cape Town with a strong focus on Python and full-stack web development. Beyond building projects, I’m genuinely passionate about teaching and mentoring—especially helping beginners understand core concepts and build confidence in their problem-solving skills.
                         I’ve worked on projects ranging from Django and React applications to machine learning systems, and I enjoy breaking down complex ideas into simple, practical explanations. "
             />
-            <meta name="google-site-verification" content="q2HC-O3rlZcv1OptYuyn_0wLtIm2_vONOTi2voR0mek" />
+            
             <meta name="keywords" content="Shine Chikwapulo, React Developer, Software Engineer, Python Developer, frontend" />
             <link rel="canonical" href="https://shine-chikwapulo.netlify.app" />
             
